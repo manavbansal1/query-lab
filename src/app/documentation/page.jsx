b28a0e5cee5c6982b0e7d94dcf40b725f0f77682
Manav Bansal <bansalmanav39@gmail.com>
2025-10-07 20:56:01 -0700

    Added underconstruction to docs page

--- src/app/documentation/page.jsx
+++ src/app/documentation/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import UnderConstruction from '@/components/UnderConstruction'
 
 const page = () => {
   return (
     <div>
-      Documentation
+      <UnderConstruction 
+      title="Documentation"
+      message="Comprehensive guides, tutorials, and examples are on the way to help you master SQL and MongoDB!"
+      estimatedTime="Coming Very Soon"
+    />
     </div>
   )
 }
