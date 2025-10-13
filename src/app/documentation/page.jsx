1764ecd2aff7a61fa3ccf571b5bf29a30c2c4844
Manav Bansal <bansalmanav39@gmail.com>
2025-10-13 15:05:08 -0700

    Cleaned up the code a bit

--- src/app/documentation/page.jsx
+++ src/app/documentation/page.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import UnderConstruction from '@/components/UnderConstruction'
 import Documentation from '@/components/Documentation'
 
 const page = () => {
   return (
     <div>
-      {/* <UnderConstruction 
-      title="Documentation"
-      message="Comprehensive guides, tutorials, and examples are on the way to help you master SQL and MongoDB!"
-      estimatedTime="Coming Very Soon"
-    /> */}
     <Documentation/>
     </div>
   )
